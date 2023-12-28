fa48066196d6cac53b5b99c1144b49fc9adc2589
Dhruv Bansal <108138649+dhruvbansal26@users.noreply.github.com>
2023-12-27 20:03:34 -0500

    changed auth

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ui/toggle-mode";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { signIn, useSession, signOut } from "next-auth/react";
+import { signIn, useSession, signOut, getProviders } from "next-auth/react";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -12,8 +12,29 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { getServerSession } from "next-auth";
+import Signin from "@/pages/auth/signin";
 
-const Navbar = () => {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getServerSession(context.req, context.res, authOptions);
+
+  // If the user is already logged in, redirect.
+  // Note: Make sure not to redirect to the same page
+  // To avoid an infinite loop!
+  if (session) {
+    return { redirect: { destination: "/" } };
+  }
+
+  const providers = await getProviders();
+
+  return {
+    props: { providers: providers ?? [] },
+  };
+}
+
+const Navbar = ({ providers }) => {
   const { data: session, status } = useSession();
 
   if (!session) {
@@ -33,7 +54,7 @@ const Navbar = () => {
               <li>
                 <Button
                   onClick={() => {
-                    signIn();
+                    Signin(providers);
                   }}
                 >
                   Login
