3f3eab31d786e32a30f6da59d8c5d3ca563ac5a0
Dhruv Bansal <108138649+dhruvbansal26@users.noreply.github.com>
2023-12-27 20:09:45 -0500

    done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ui/toggle-mode";
